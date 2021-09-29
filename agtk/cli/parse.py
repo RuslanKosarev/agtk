82cefa8e757366922bb8a277e2aaf1593d7d9413
korus <ruslan.kosarev@smedx.com>
2021-09-29 13:02:08 +0300

    parse

---
+++ agtk/cli/parse.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+""" Read and print meta data.
+"""
+
+import click
+from pathlib import Path
+import numpy as np
+
+from agtk import dataset
+
+
+@click.command()
+@click.option('-p', '--path', type=Path, required=True,
+              help='Path to directory to parse.')
+def parse(path: Path):
+
+    in_path = path.expanduser()
+
+    paths = list(in_path.rglob('*'))
+    paths.sort()
+
+    for path in paths:
+        if path in in_path.glob('*'):
+            print('\n')
+        print(path)
+
+
+if __name__ == '__main__':
+    parse()
